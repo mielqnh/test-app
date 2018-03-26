d4cb0085db77ed7822e9dcea97d2aeb31eab0ce7
Miel <miel.noelanders@qnh.Be>
2018-03-26 14:32:40 +0200

    alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import {SuccessAlertComponent} from './successalert/successalert.component';
 
 
 @NgModule({
   declarations: [/*Nieuwe components toevoegen doe je hier.*/
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
